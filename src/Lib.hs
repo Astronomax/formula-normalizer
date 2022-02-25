7ea4432ebbbfdfe44e01a0e267b0b41c0fa5330a
Astronomax <fgfgfb93@gmail.com>
2022-02-25 21:07:13 +0300

    Add command line args parsing

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,10 +5,37 @@ import Parser (parseFormula)
 import FormulaToNNF (formulaToNNF)
 import FormulaToDNF (formulaToDNF)
 import FormulaToCNF (formulaToCNF)
+import System.Environment (getArgs)
 import Data.Either
+import Control.Monad.Except (Except, runExcept, MonadError (throwError))
+
+data MainError = InvalidArguments | ParsingError
+
+instance Show MainError where
+    show InvalidArguments = "InvalidArguments"
+    show ParsingError     = "ParsingError"
+  
+mainExcept :: [String] -> Except MainError Formula
+mainExcept args = do
+    strFormula <- argsExcept args
+    f <- parseExcept strFormula
+    return f
+    where   argsExcept :: [String] -> Except MainError String
+            argsExcept [arg] = return arg
+            argsExcept args = throwError $ InvalidArguments
+
+            parseExcept :: String -> Except MainError Formula
+            parseExcept strFormula = case parseFormula strFormula of
+                Right f -> return f
+                Left  s -> throwError ParsingError
 
 someFunc :: IO ()
 someFunc = do
-    s <- getLine
-    let (Right f) = parseFormula s
-    putStrLn $ show $ formulaToCNF f
+    args <- getArgs
+    case runExcept $ mainExcept args of
+        Right f -> do 
+            putStrLn $ "NNF: " ++ (show $ formulaToNNF f)
+            putStrLn $ "DNF: " ++ (show $ formulaToDNF f)
+            putStrLn $ "CNF: " ++ (show $ formulaToCNF f)
+        Left err -> print err
+
---
+++ src/Tseitin.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Tseitin where
+
+import Formula
+
+tseitin :: Formula -> Formula
+tseitin = id
